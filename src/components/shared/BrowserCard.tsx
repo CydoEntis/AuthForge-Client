2df6edf5241c2848345b9a2f0575ba4351e78264
Cody <cydoentis@gmail.com>
2025-11-08 18:01:44 -0500

    feat(browser-card): added a button to activate/deactive apps and also delete apps

--- src/components/shared/BrowserCard.tsx
+++ src/components/shared/BrowserCard.tsx
@@ -1,4 +1,6 @@
+import { Power, X } from "lucide-react";
 import type { ReactNode } from "react";
+import { Button } from "../ui/button";
 
 interface AppCardProps {
   description?: string;
@@ -19,14 +21,14 @@ export function BrowserCard({
   googleEnabled,
   githubEnabled,
   isOnline = false,
-  maxWidth = 300,
+  maxWidth = 350,
   children,
   className = "",
 }: AppCardProps) {
   return (
     <div className={`relative group cursor-pointer min-h-[280px] ${className}`} style={{ maxWidth }}>
       {/* Outer Card */}
-      <div className="relative w-full h-full flex flex-col rounded-xl overflow-hidden border border-input/70 shadow-md transition-all duration-300 group-hover:border-input">
+      <div className="relative w-full h-full flex flex-col rounded-xl overflow-hidden border border-input/70  transition-all duration-300 group-hover:border-input">
         {/* Top bar */}
         <div className="flex items-center gap-2 px-3 py-2 bg-sidebar">
           <div className="flex gap-1.5">
@@ -36,6 +38,9 @@ export function BrowserCard({
           </div>
           <div className="flex-1 text-center text-xs sm:text-sm px-2 py-[2px] rounded-full truncate bg-input">
             {url}
+          </div>
+          <div className="bg-card hover:bg-red-500/10 w-5 h-5 rounded flex justify-center items-center">
+            <X size={16} className="text-muted-foreground hover:text-red-900" />
           </div>
         </div>
 
@@ -48,11 +53,18 @@ export function BrowserCard({
       {/* Tag */}
       <div className="absolute right-0 bottom-0 bg-background border-t border-l border-input rounded-tl-xl flex items-center justify-center w-42 pt-2 pl-2 transition-colors duration-300 group-hover:border-input">
         <p className="flex items-center gap-2 text-sm font-semibold w-full justify-center p-2 rounded-tl-xl rounded-br-xl border border-input bg-linear-to-br from-card to-background transition-all duration-300  group-hover:border-input group-hover:to-primary/10">
-          <span className="relative w-2 h-2 rounded-full">
-            <span className={`absolute inset-0 rounded-full ${isOnline ? "bg-primary" : "bg-muted-foreground"}`} />
-            {isOnline && <span className="absolute inset-0 rounded-full bg-primary/60 animate-ping" />}
-          </span>
-          {isOnline ? "Active" : "Inactive"}
+          <div className="flex justify-between items-center w-full">
+            <div className="relative w-full flex items-center gap-2">
+              <span className="relative w-2 h-2 rounded-full block">
+                <span className={`absolute inset-0 rounded-full ${isOnline ? "bg-primary" : "bg-muted-foreground"}`} />
+                {isOnline && <span className="absolute inset-0 rounded-full bg-primary/60 animate-ping" />}
+              </span>
+              {isOnline ? "Active" : "Inactive"}
+            </div>
+            <Button variant="outline" size="sm" className="p-1! px-2!">
+              <Power className="text-muted-foreground" />
+            </Button>
+          </div>
         </p>
       </div>
     </div>
