4f42255aa3e4ef8442e32a8f05b2f464c8989f2f
Cody <cydoentis@gmail.com>
2025-11-11 13:16:39 -0500

    refactor(loading-button): Loading button can now take custom class names

--- src/components/shared/LoadingButton.tsx
+++ src/components/shared/LoadingButton.tsx
@@ -4,17 +4,20 @@ import { Spinner } from "../ui/spinner";
 interface LoadingButtonProps extends React.ComponentProps<typeof Button> {
   isLoading?: boolean;
   loadingText?: string;
+  className?: string;
   children: React.ReactNode;
 }
 
 export function LoadingButton({
   isLoading = false,
   loadingText = "Loading...",
+  className,
   children,
+
   ...props
 }: LoadingButtonProps) {
   return (
-    <Button disabled={isLoading} {...props} className="">
+    <Button disabled={isLoading} {...props} className={className}>
       {isLoading ? (
         <>
           <Spinner className="mr-2 inline-block" /> {loadingText}
