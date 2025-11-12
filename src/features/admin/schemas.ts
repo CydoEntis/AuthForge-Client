deca95ae85e4fbed0bcc071c93ef65fb2b6ca4ee
Cody <cydoentis@gmail.com>
2025-11-12 10:59:38 -0500

    fix(admin-schemas): login admin schema now properly validates password

--- src/features/admin/schemas.ts
+++ src/features/admin/schemas.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const loginAdminSchema = z.object({
   email: z.email({ message: "Please enter a valid email address." }),
-  password: z.string().min(0, { message: "Password cannot be empty" }),
+  password: z.string().min(1, { message: "Password cannot be empty" }),
 });
 
 export const forgotPasswordAdminSchema = z.object({
