d6fe95afceba7798ca069a6d686765b5d6fba939
Cody <cydoentis@gmail.com>
2025-11-13 19:16:20 -0500

    refactor: updated form error to match style of the app

--- src/components/shared/FormError.tsx
+++ src/components/shared/FormError.tsx
@@ -1,16 +1,14 @@
-import { Alert, AlertTitle } from "../ui/alert";
-import { CircleAlert } from "lucide-react";
-
 type FormErrorProps = {
   message: string;
 };
 
 function FormError({ message }: FormErrorProps) {
   return (
-    <Alert variant="destructive" className="bg-red-500/10! border-red-500/30">
-      <CircleAlert />
-      <AlertTitle>{message}</AlertTitle>
-    </Alert>
+    <div className="flex justify-center">
+      <div className="flex items-center justify-between gap-4 text-sm px-6 py-3 rounded-lg transition-all duration-300 font-semibold inset-shadow-failure bg-linear-to-t from-red-400/10 to-red-400/40 text-red-500 border border-red-500/30">
+        <span>{message}</span>
+      </div>
+    </div>
   );
 }
 
