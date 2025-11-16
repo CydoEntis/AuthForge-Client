20bf1d4702da56ae6715101184ad21ebe7e42dfd
Cody <cydoentis@gmail.com>
2025-11-16 13:50:30 -0500

    refactor(admin-change-password): admin change password is now a separated form not a card like admin login, reset password etc.

--- src/features/admin/components/AdminChangePassword.tsx
+++ src/features/admin/components/AdminChangePassword.tsx
@@ -1,4 +1,3 @@
-import { CardWithLogo } from "@/components/CardWithLogo";
 import { Form } from "@/components/ui/form";
 import { FormInput } from "@/components/shared/FormInput";
 import FormError from "@/components/shared/FormError";
@@ -7,45 +6,42 @@ import { useAdminForgotPasswordForm } from "../hooks/useAdminForgotPasswordForm"
 
 export function AdminChangePassword() {
   const { form, handleSubmit, isLoading } = useAdminForgotPasswordForm();
-  // Todo: This will prob get moved to a modal with all other admin update related content.
   return (
-    <CardWithLogo title="Change Password" subText="Update your password">
-      <Form {...form}>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="space-y-1">
-            <FormInput
-              form={form}
-              name="newPassword"
-              label="New Password"
-              placeholder="********"
-              type="password"
-              isLoading={isLoading}
-            />
-            <FormInput
-              form={form}
-              name="newPassword"
-              label="New Password"
-              placeholder="********"
-              type="password"
-              isLoading={isLoading}
-            />
-            <FormInput
-              form={form}
-              name="confrimNewPassword"
-              label="Confirm New Password"
-              type="password"
-              placeholder="********"
-              isLoading={isLoading}
-            />
-          </div>
+    <Form {...form}>
+      <form onSubmit={handleSubmit} className="space-y-6">
+        <div className="space-y-1">
+          <FormInput
+            form={form}
+            name="currentPassword"
+            label="Current Password"
+            placeholder="********"
+            type="password"
+            isLoading={isLoading}
+          />
+          <FormInput
+            form={form}
+            name="newPassword"
+            label="New Password"
+            placeholder="********"
+            type="password"
+            isLoading={isLoading}
+          />
+          <FormInput
+            form={form}
+            name="confrimNewPassword"
+            label="Confirm New Password"
+            type="password"
+            placeholder="********"
+            isLoading={isLoading}
+          />
+        </div>
 
-          {form.formState.errors.root && <FormError message={form.formState.errors.root.message as string} />}
+        {form.formState.errors.root && <FormError message={form.formState.errors.root.message as string} />}
 
-          <LoadingButton type="submit" className="w-full mt-8" isLoading={isLoading} loadingText="Sending Email...">
-            Change Password
-          </LoadingButton>
-        </form>
-      </Form>
-    </CardWithLogo>
+        <LoadingButton type="submit" className="w-full mt-8" isLoading={isLoading} loadingText="Sending Email...">
+          Change Password
+        </LoadingButton>
+      </form>
+    </Form>
   );
 }
