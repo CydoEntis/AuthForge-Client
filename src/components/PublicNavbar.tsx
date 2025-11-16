e97b9e6ed7657c51c3330ff435ef3d52fe2c9c22
Cody <cydoentis@gmail.com>
2025-11-16 13:48:32 -0500

    refactor(navbar and form error): navbar now includes a logo and form error is now simpler

--- src/components/PublicNavbar.tsx
+++ src/components/PublicNavbar.tsx
@@ -1,7 +1,10 @@
+import { Logo } from "./Logo";
+
 export function PublicNavbar() {
   return (
     <div className="p-4 inset-shadow-sm bg-linear-to-t from-card to-background rounded-lg flex justify-between items-center border dark:border-black border-[#c7c7c7]">
       <div className="flex gap-2 items-center">
+        <Logo size={18} />
         <h3 className="font-bold text-lg text-muted-foreground">Auth Forge</h3>
       </div>
       {/* Currently we dont want to manage a light mode */}
--- src/components/shared/FormError.tsx
+++ src/components/shared/FormError.tsx
@@ -4,10 +4,8 @@ type FormErrorProps = {
 
 function FormError({ message }: FormErrorProps) {
   return (
-    <div className="flex justify-center">
-      <div className="flex items-center justify-between gap-4 text-sm px-6 py-3 rounded-lg transition-all duration-300 font-semibold inset-shadow-failure bg-linear-to-t from-red-400/10 to-red-400/40 text-red-500 border border-red-500/30">
-        <span>{message}</span>
-      </div>
+    <div className="gap-4 text-sm px-6 py-3 rounded-lg transition-all duration-300 font-semibold  shadow-sm bg-linear-to-t from-red-500/10 to-red-500/30 text-red-500 border border-red-500/30 w-full text-center">
+      <p>{message}</p>
     </div>
   );
 }
